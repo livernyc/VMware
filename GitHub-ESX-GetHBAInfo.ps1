403e01d37e3a125995cc3835ea54871d7e7088a2
livernyc <chris_lamorte@hotmail.com>
2022-02-28 18:50:56 -0500

    test

--- GitHub-ESX-GetHBAInfo.ps1
+++ GitHub-ESX-GetHBAInfo.ps1
@@ -1,3 +1,5 @@
 # Get WWN in vSphere cluster
 
 Get-Cluster Your-Cluster-Name | Get-VMhost | Get-VMHostHBA -Type FibreChannel | Select VMHost,Device,@{N="WWN";E={"{0:X}" -f $_.PortWorldWideName}} | Sort VMhost,Device | export-csv Cluster_HBA_Info.csv
+
+# Test
